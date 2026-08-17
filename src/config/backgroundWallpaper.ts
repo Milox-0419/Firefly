155acc802924ccc83711ff0968c8c9959d2af333
Milox-0419 <yxy120419@gmail.com>
2026-08-17 22:27:15 +0800

    fix:修复动态壁纸无法加载问题

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -39,7 +39,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/background.mp4",
+			"assets/images/DesktopWallpaper/desktop.mp4",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
