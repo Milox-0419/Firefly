479185afb621c7d01c5bcef28405f1a3359d2ca1
Milox-0419 <yxy120419@gmail.com>
2026-08-17 13:51:34 +0800

    fix:修改pages cms配置

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -78,7 +78,7 @@ export const siteConfig: SiteConfig = {
 			type: "image",
 			value: "assets/images/logo/Hacker-light.png",
 			valueDark: "assets/images/logo/Hacker-dark.png",
-			alt: "🍀",
+			alt: "a",
 		},
 		// 导航栏标题
 		title: "Milox's Blog",
@@ -87,7 +87,7 @@ export const siteConfig: SiteConfig = {
 		// 导航菜单对齐方式，left：左对齐，center：居中
 		menuAlign: "center",
 		// 导航栏图标和标题是否跟随主题色
-		followTheme: false,
+		followTheme: true,
 		// 导航栏是否固定在顶部并始终可见
 		stickyNavbar: true,
 	},
